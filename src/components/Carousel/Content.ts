a9bb240d41575b307d4e8da15f7a9e2827187770
Brspontes <brian.robert16@hotmail.com>
2021-08-15 19:11:51 -0300

    finalizando pagina incial

---
+++ src/components/Carousel/Content.ts
@@ -0,0 +1,24 @@
+interface contentCarousel {
+  urlImg: string
+  continente: string
+  comentario: string
+}
+
+export const content: contentCarousel[] = [
+  {
+    urlImg: 'https://bigseventravel.com/wp-content/uploads/2019/12/marco-ten-donkelaar-tTolSho3tBY-unsplash-1024x768.jpg',
+    comentario: 'O continente mais antigo',
+    continente: 'Europa'
+  },
+  {
+    urlImg: 'https://bigseventravel.com/wp-content/uploads/2019/03/riydh.jpg',
+    comentario: 'O continente mais antigo',
+    continente: 'Ásia'
+  },
+  {
+    urlImg: 'https://bigseventravel.com/wp-content/uploads/2019/03/macau.jpg',
+    comentario: 'O continente mais antigo',
+    continente: 'Americano'
+  }
+]
+
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,15 +1,13 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from 'swiper/core';
-
+import SwiperCore, { Autoplay, Pagination } from 'swiper/core';
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
-
 import styles from './styles.module.scss';
-
-
-
+import { content } from "./Content";
+import { Text } from "@chakra-ui/react";
 
 export default function Carousel () {
+  SwiperCore.use([Autoplay])
   SwiperCore.use([Pagination]);
   return (
     <Swiper
@@ -18,14 +16,15 @@ export default function Carousel () {
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
+      autoplay={{ delay: 5000 }}
     >
-      <SwiperSlide className={styles.swiperslide}>
-        <p>TESTE</p>
-        <img src="https://bigseventravel.com/wp-content/uploads/2019/12/marco-ten-donkelaar-tTolSho3tBY-unsplash-1024x768.jpg" alt="" />
-      </SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}>Slide 2</SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}>Slide 3</SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}>Slide 4</SwiperSlide>
+      {content.map(c => (
+        <SwiperSlide className={styles.swiperslide}>
+          <Text borderRadius="10" backgroundColor="gray.800" color="white" opacity="70%" fontSize="5xl" position="absolute" >{c.continente}</Text>
+          <Text color="white" opacity="70%" fontSize="5xl" position="absolute" >{c.continente}</Text>
+          <img src={c.urlImg} alt="continente" />
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 }
